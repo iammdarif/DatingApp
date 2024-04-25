f8dc2486b1f3ad5602629ab9212dc42f02d62968
ALVIRA-ARIF\Arif <mohammad.arif@health.telstra.com>
2024-04-25 17:19:42 +1000

    end of section 8

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -39,6 +39,7 @@ public class TokenService : ITokenService
         var token = tokenHandler.CreateToken(tokenDescriptor);
         
         return tokenHandler.WriteToken(token);
+        
 
     }
 }
